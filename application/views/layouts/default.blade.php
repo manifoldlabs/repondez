36a6d4960ff91bae81a320db260cc19850df2ca6
alexford <alex@cratr.co>
2013-01-24 16:27:10 -0700

    Adding bootstrap, basic layout

---
+++ application/views/layouts/default.blade.php
@@ -0,0 +1,19 @@
+<!doctype html>
+<html lang="en">
+  <head>
+    <title>Répondez: @if (isset($title)) {{$title}} @endif</title>
+
+    <!-- Styles -->
+    <link href="/css/application.css" rel="stylesheet">
+  </head>
+  <body>
+    @include('partials.header')
+    
+    <div class='container'>
+      <h1>Hello, world!</h1>
+    </div>
+    
+
+    <script src="/js/bootstrap.min.js"></script>
+  </body>
+</html>
---
+++ application/views/partials/header.blade.php
@@ -0,0 +1,8 @@
+<div class="navbar navbar-fixed-top">
+  <div class="navbar-inner"><div class='container'>
+    <a class="brand" href="/">Répondez</a>
+    <ul class="nav">
+      <li class="active"><a href="/">Home</a></li>
+    </ul>
+  </div></div>
+</div>
