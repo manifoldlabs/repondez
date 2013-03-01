bbc5718287115597209170ad16dfa5a7cf4da4f8
alexford <alex@cratr.co>
2013-02-28 19:20:12 -0700

    Initial migration (users, events, invitations)

---
+++ application/migrations/2013_03_01_020953_initial_migration.php
@@ -0,0 +1,76 @@
+<?php
+
+class Initial_Migration {
+
+	/**
+	 * Make changes to the database.
+	 *
+	 * @return void
+	 */
+	public function up()
+	{
+		// users table
+		Schema::create('users', function($table) {
+	    $table->increments('id');
+
+	    $table->string('email', 320);
+	    $table->string('password', 256);
+	    $table->integer('role');
+
+	    // created_at | updated_at DATETIME
+	    $table->timestamps();  
+		});
+
+		// events table
+		Schema::create('events', function($table) {
+	    $table->increments('id');
+
+	    $table->string('name',256);
+
+	    $table->integer('user_id');
+	    $table->date('date');
+
+	    $table->integer('access_code');
+	    $table->string('access_number',10);
+
+	    // created_at | updated_at DATETIME
+	    $table->timestamps();  
+		});
+
+		// invitations table
+		Schema::create('invitations', function($table) {
+	    $table->increments('id');
+
+	    $table->string('name',256);
+
+	    $table->string('status',16);
+	    $table->string('rsvp',5);
+
+	    $table->integer('count_expected');
+	    $table->integer('count');
+
+	    $table->integer('event_id');
+	    $table->integer('user_id');
+
+	    $table->integer('access_code');
+	    $table->string('access_number',10);
+
+	    // created_at | updated_at DATETIME
+	    $table->timestamps();  
+		});
+	}
+
+	/**
+	 * Revert the changes to the database.
+	 *
+	 * @return void
+	 */
+	public function down()
+	{
+		// drop users
+		Schema::drop('users');
+		Schema::drop('events');
+		Schema::drop('invitations');
+	}
+
+}
